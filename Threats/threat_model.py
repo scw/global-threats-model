062c4880b875cbbe210f1cfe4ac8942de7aec62b
Matthew Perry <perrygeo@gmail.com>
2006-09-27 21:06:25 +0000

    refactored threat model into two steps. first generate the threat/habitat combo layers, then weight and add them. Plus a matrix csv example

--- Threats/threat_model.py
+++ Threats/threat_model.py
@@ -17,31 +17,49 @@
 """
 
 import sys
+import os
 
 def parseMatrix(matrix_file):
-    matrix = {
-              'threat1': {'habitat1': 4, 'habitat2':2} ,
-              'threat2': {'habitat1': 3, 'habitat2':1} 
-             }
-    # TODO read csv
-    # TODO put into dictionary
-    #      (if something goes wrong, throw exception)
+    try:
+        fh = open(matrix_file, 'r')
+        lines = fh.readlines()
+    except:
+        print "can't read matrix file"
+        sys.exit(1)
+
+    habitats = lines[0].replace('\n','').split(',')[1:]
+    matrix = {}
+    for i in (range(1,len(lines))):
+        line = lines[i].replace('\n','').split(',')
+        threat = line[0]
+        weights = {}
+        for j in (range(1,len(habitats))):
+            weights[habitats[j]] = float(line[j])
+
+        matrix[threat] = weights
+
+    print
+    print matrix
+    print
     return matrix
 
-def processThreatHabitatCombo(threat,habitat,weight):
-    # TODO make sure both layers exist
-    # TODO mapcalc em together
-    valid = True
-    output = threat + "_" + habitat
-    return {'valid': valid, 'output': output }
-
-def processAllCombos(combos):
-    output_map = "model_output"
+def processAllCombos(matrix,output_map):
+    weighted_terms = []
+    for threat in matrix.keys():
+        for habitat in matrix[threat].keys():
+            #TODO check that combo layer exists
+            input = "combo_" + threat + "_" + habitat
+            weight = matrix[threat][habitat]
+            weighted_terms.append("(%s * %s)" % (weight, input) )
     mapcalc  = " r.mapcalc %s = \"" % output_map
-    mapcalc += ' + '.join(combos)
+    mapcalc += ' + '.join(weighted_terms)
     mapcalc += "\""
-    # TODO  Check if everything went OK
     print mapcalc
+
+    response = os.popen(mapcalc).read().rstrip()
+    print response
+
+    # TODO check validity of output map
     valid = True
     return {'valid': valid, 'output': output_map} 
         
@@ -49,26 +67,15 @@ def processAllCombos(combos):
 if __name__ == "__main__":
     try:
         matrix_file = sys.argv[1]
+        output_map = sys.argv[2]
     except:
-        # TODO check for valid file
-        #sys.exit(1)
-        pass
+        print "usage: threat_model.py matrix_file.txt output_grass_map"
+        sys.exit(1)
 
     matrix = parseMatrix(matrix_file)
-
-    threat_combos = []
-
-    for threat in matrix.keys():
-        for habitat in matrix[threat].keys():
-            response = processThreatHabitatCombo(threat,habitat,matrix[threat][habitat])
-            if response['valid']:
-                threat_combos.append(response['output'])
-                print response['output']    
-                print " Weighting = %s " % matrix[threat][habitat]
-            else:
-                print "%s / %s  combination failed" % (threat, habitat)
-
-    response = processAllCombos(threat_combos)
+    response = processAllCombos(matrix,output_map)
     if response['valid']:
         print "Your model result is ... '%s'" % response['output']
+
+
 
