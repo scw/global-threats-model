3399de14679b4eaa81f583e69f537cd7c9410773
Shaun Walbridge <shaun.walbridge@gmail.com>
2007-01-23 21:59:32 +0000

    redeveloped script to be easier to operate and incorporate land-sea mask changes

--- Basins/basinBump.py
+++ Basins/basinBump.py
@@ -1,11 +1,15 @@
 # basinBump.py
 # Rebuilds an elevation model with breaks (increase elevation), burns (decrease elevation)
-# and drains (assigned nodata locations).  These forcings allow an accurate hydrological
-# deliniation for the elevation surface.
+# and drains (assign nodata locations).  These forcings allow an accurate hydrological
+# deliniation for the elevation surface. Also allows changes in the 
 #
 # Author: scw <walbridge@nceas.ucsb.edu>
 # Date: 3.27.2006
-# Usage: basinBump <elevation> <breaks_shp> <burns_shp> <drains_shp> <output dir> <output prefix> 
+# Usage: basinBump <prefix> <elevation> <modifications dir> <output dir>
+# 
+# Usage Example: The DEM is named "eu_ult_clip" and the modifications folder is "modifications".
+#                You want to output the results to the "build_try1" folder:
+#                basinBump.py eu c:\dev\eu\eu_ult_clip c:\dev\eu\mods c:\dev\eu\build_try1
 
 # Import system modules
 import sys, string, os, win32com.client
@@ -15,73 +19,86 @@ gp = win32com.client.Dispatch("esriGeoprocessing.GpDispatch.1") # Create the geo
 gp.CheckOutExtension("spatial")                                 # Check out the required license
 gp.overwriteoutput = 1                                          # Overwrite existing files
 
-# Load required toolboxes...
-#gp.AddToolbox("F:/gis/a9/ArcGIS/ArcToolbox/Toolboxes/Data Management Tools.tbx")
-#gp.AddToolbox("F:/gis/a9/ArcGIS/ArcToolbox/Toolboxes/Spatial Analyst Tools.tbx")
-#gp.AddToolbox("F:/gis/a9/ArcGIS/ArcToolbox/Toolboxes/Conversion Tools.tbx")
+if len(sys.argv) < 4:
+	print "Usage: basinBump <prefix> <elevation> <modifications dir> <output dir>"
+	sys.exit()
 
-#if len(sys.argv) < 6:
-#	print "Usage: basinBump <elevation> <infill_shp> <breaks_shp> <burns_shp> <drains_shp> <output dir> <output prefix>"
-#	sys.exit()
-
-try:	
-    demarg = sys.argv[1]
-    infillarg = sys.argv[2]
-    breaksarg = sys.argv[3]
-    burnsarg = sys.argv[4]
-    drainsarg = sys.argv[5]
-    outputDir  = sys.argv[6]
-    outputName = sys.argv[7]
+try:
+    
+    prefix = sys.argv[1]
+    demarg = sys.argv[2]
+    modDir = sys.argv[3]
+    outputDir  = sys.argv[4]
+    
 except:
     ########## EDIT THESE IF YOU'RE LAZY AND DONT WANT TO TYPE CL PARAMS #######
-    outputName = "af"	
-    outputDir  = "C:\\WorkSpace\\srtm_basins\\af_basins\\build_try4"
-    ### and to a lesser extent, these
-    demarg = "C:\\WorkSpace\\srtm_basins\\af_basins\\af_ult_fix2"
-    infillarg = "C:\\WorkSpace\\srtm_basins\\af_basins\\af_fills.shp"
-    breaksarg = "C:\\WorkSpace\\srtm_basins\\af_basins\\af_breaks.shp"
-    burnsarg = "C:\\WorkSpace\\srtm_basins\\af_basins\\af_burns.shp"
-    drainsarg = "C:\\WorkSpace\\srtm_basins\\af_basins\\af_drains.shp"
+    prefix = "as_se"	
+    outputDir  = "F:\\dev\wgs_globe\\asia\\as_dev2\\tmp"
+    demarg = "F:\\dev\wgs_globe\\asia\\as_dev2\\tmp"
+    modDir = ""
     ###########
 
+# clean up directories
+paths = {'mod' : modDir, 'out' : outputDir}
+for name, path in paths.items():
+    if path.endswith('\\'):
+        paths[name] = paths[name][:-1]
+    paths[name] += "\\" + prefix
 
-outputBase = outputDir + "\\" + outputName
+modElem = ['breaks', 'burns', 'fill', 'fill_area', 'null', 'null_area']
+inElem = { 'DEM'   : demarg, }
 
-inElem = { 'DEM'   : demarg, 'breaks' : breaksarg, \
-           'burns' : burnsarg, 'drains' : drainsarg, 'infill': infillarg}
-
-for elemName, elemPath in inElem.items():
-    (path, filename) = os.path.split(elemPath)
-    if path == '':
-        inElem[elemName] = outputDir + "\\" + filename
-
+for mod in modElem:
+    inElem[mod] = "%s_%s.shp" % (paths['mod'], mod)
+    
 # Extent and cellsize should match input DEM
 gp.extent   = inElem['DEM']
 gp.cellsize = inElem['DEM']
 
-'''print outputBase
 print inElem
-sys.exit()
-'''
 
 # output data
-infill_0 = outputBase + '_if0'
-infill_1 = outputBase + '_if1'
-break_0 = outputBase + '_bk0'
-burn_0  = outputBase + '_bn0'
-drain_0 = outputBase + '_dn0'
-drain_1 = outputBase + '_dn1'
-drain_2 = outputBase + '_dn2'
-drain_3 = outputBase + '_dn3'
-combine = outputBase + '_dem'
+infill_0 = paths['out'] + '_if0'
+infill_1 = paths['out'] + '_if1'
+fill_0 = paths['out'] + '_fi0'
+fill_1 = paths['out'] + '_fi1'
+innulls_0 = paths['out'] + '_in0'
+innulls_1 = paths['out'] + '_in1'
+innulls_2 = paths['out'] + '_in2'
+innulls_3 = paths['out'] + '_in3'
+nulls_0 = paths['out'] + '_nf0'
+nulls_1 = paths['out'] + '_nf1'
+nulls_2 = paths['out'] + '_nf2'
+nulls_3 = paths['out'] + '_nf3'
+break_0 = paths['out'] + '_bk0'
+burn_0  = paths['out'] + '_bn0'
+combine = paths['out'] + '_dem'
 
 try:
-    print "Infilling the DEM"
-    # Process: Feature to Raster...
-    gp.FeatureToRaster_conversion(inElem['infill'], "Id", infill_0, gp.cellsize)
-    gp.SingleOutputMapAlgebra_sa("con( isnull( %s ), %s, %s )" \
-                                 % (inElem['DEM'], infill_0, inElem['DEM']), infill_1)
+    print "Rasterizing fill-in (area)"
+    gp.FeatureToRaster_conversion(inElem['fill_area'], 1, infill_0, gp.cellsize)
+    gp.SingleOutputMapAlgebra_sa("con( isnull( %s ), %s, %s)" % \
+                                 (inElem['DEM'], infill_0, inElem['DEM']), infill_1)
 
+    print "Rasterizing fills (point)"
+    gp.FeatureToRaster_conversion(inElem['fill'], 1, fill_0, gp.cellsize)
+    gp.SingleOutputMapAlgebra_sa("con( isnull( %s ), %s, %s)" % \
+                                (infill_1, fill_0, infill_1), fill_1)
+    
+    print "Rasterizing nulls (area)"
+    gp.FeatureToRaster_conversion(inElem['null_area'], 0, innulls_0, gp.cellsize)
+
+    gp.IsNull_sa(innulls_0, innulls_1)
+    gp.Con_sa(innulls_1, '0', innulls_2, '1', "")
+    gp.SetNull_sa(innulls_2, '0', innulls_3, "")
+                                     
+    print "Raserizing nulls (point)"
+    gp.FeatureToRaster_conversion(inElem['null'], 0, nulls_0, gp.cellsize)
+  
+    gp.IsNull_sa(nulls_0, nulls_1)
+    gp.Con_sa(nulls_1, '0', nulls_2, '1', "")
+    gp.SetNull_sa(nulls_2, '0', nulls_3, "")
+                                  
     print "Rasterizing breaks"
     # Process: Feature to Raster...
     gp.FeatureToRaster_conversion(inElem['breaks'], "Id", break_0, gp.cellsize)
@@ -89,23 +106,11 @@ try:
     print "Rasterizing burns"
     # Process: Feature to Raster (2)...
     gp.FeatureToRaster_conversion(inElem['burns'], "Id", burn_0, gp.cellsize)
-
-    print "Rasterizing drains"
-    # Process: Feature to Raster (3)...
-    gp.FeatureToRaster_conversion(inElem['drains'], "Id", drain_0, gp.cellsize)
-
-    # Process: Is Null...
-    gp.IsNull_sa(drain_0, drain_1)
-
-    # Process: Con...
-    gp.Con_sa(drain_1, '0', drain_2, '1', "")
-
-    # Process: Set Null...
-    gp.SetNull_sa(drain_2, '0', drain_3, "")
-
+ 
     print "Combining layers into one modified DEM"
     # Process: Single Output Map Algebra...
-    gp.SingleOutputMapAlgebra_sa("con(isnull(%s), 0, 200) + con(isnull(%s), 0, -400) + %s + %s" % (break_0, burn_0, drain_3, infill_1), combine)
+    gp.SingleOutputMapAlgebra_sa("con(isnull(%s), 0, 200) + con(isnull(%s), 0, -400) + %s + %s + %s" %  \
+                                 (break_0, burn_0, nulls_3, innulls_3, fill_1), combine)
 
     print "Building Pyrimids"
     # Process: Build Pyramids...
