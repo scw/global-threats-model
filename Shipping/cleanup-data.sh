50eb7c26b1313346b9c6044b859ace398799985f
Shaun Walbridge <shaun.walbridge@gmail.com>
2007-05-17 22:11:12 +0000

    added shipping lines, one line per point pair, cleaned up and reran

--- Shipping/cleanup-data.sh
+++ Shipping/cleanup-data.sh
@@ -4,21 +4,24 @@
 # Run as the postgres user or with a .pgpass file
 
 # Import the data into PostGIS
-shp2pgsql -d input/mw_shipping/xbt_r05all_point.shp shipping | psql -d gisdata
+#shp2pgsql -d input/mw_shipping/xbt_r05all_point.shp shipping | psql -d gisdata
+shp2pgsql -d input/srtm30_mask/srtm_mask_neg_buffer_dissolve.shp srtm30_mask | psql -d gisdata
 
 # Strip the bad data from our table (verified buoy data)
-psql gisdata < input/buoys.sql
+#psql gisdata < input/buoys.sql
 
 # Add necessary tables for data cleanup (line, point tables)
-psql gisdata < input/cleanup.sql
+#psql gisdata < input/cleanup.sql
 
 # Run python code to process ship tracks into line segments
 # dumps results into two tables:
 # ship_lines, ship_points
-python shipping_tracks.py
+#python shipping_tracks.py
 
 # Dump the results to a new shapefile
-pgsql2shp -f output/shipping-lines.shp gisdata ship_lines
-pgsql2shp -f output/shipping-points.shp gisdata ship_points
-cp "output/WGS 1984.prj" output/shipping-lines.prj
-cp "output/WGS 1984.prj" output/shipping-points.prj
+#pgsql2shp -f output/shipping-lines.shp gisdata ship_lines
+#pgsql2shp -f output/shipping-points.shp gisdata ship_points
+#cp "output/WGS 1984.prj" output/shipping-lines.prj
+#cp "output/WGS 1984.prj" output/shipping-points.prj
+
+
--- Shipping/shipping_tracks.py
+++ Shipping/shipping_tracks.py
@@ -9,6 +9,15 @@ Author: Shaun C. Walbridge
 
 import sys, math, psycopg2
 
+def commit(sql):
+    try:
+        c.execute(sql)
+        #print "inserted %s" % id
+    except:
+        conn.rollback()
+        print "borked, trying %s" % sql
+    conn.commit()
+
 # connect to PostGIS database
 dbc = "dbname=gisdata user=postgres host=localhost"
 try:
@@ -25,74 +34,85 @@ r = c.fetchall()
 for row in r:
     id = row[0] # ship track id
     geom = []
+    dates = []
 
     # get point locations to build linestring
-    sql = """SELECT lon, lat
+    sql = """SELECT lon, lat, date 
              FROM shipping_clean 
-             WHERE id = '%s'
-             ORDER BY date
-          """ % id
+             WHERE id = '%s' 
+             ORDER BY date """ % id
     c.execute(sql)
     q = c.fetchall()
     count = len(q)
 
-    (prev_lon, prev_lat) = q[0]
-    for point in q:
-        # check for dateline 'wrapping'
-        (cur_lon, cur_lat) = point
-        if abs(cur_lon - prev_lon) > 180 and \
-           str(cur_lon * prev_lon)[0] is '-':
-            # interpolate two points at the 180/-180 boundry
-
-            cur_s, prev_s = 180, 180 # signs for longitude at boundry
-            if prev_lon < 0:
-                p_lon = prev_lon + 360
-                prev_s *= -1
-            else:
-                p_lon = prev_lon
-            if cur_lon < 0:
-                c_lon = cur_lon + 360
-                cur_s *= -1
-            else:
-                c_lon = cur_lon
-            slope = (prev_lat - cur_lat)/(p_lon - c_lon)
-            s_lat = slope*(180 - p_lon) + prev_lat
-            
-            # generate new points based on bountry split
-            p = "%s %s,%3.9f %2.10f),(%3.9f %2.10f,%s %s" % \
-                (prev_lon, prev_lat, prev_s, s_lat, cur_s, s_lat, cur_lon, cur_lat)
-            geom.append(p)
-
-        else:
-            # push lon lat pair onto geom
-            geom.append('%s %s' % point)
-        # set lat / lon for next run
-        (prev_lon, prev_lat) = point
-
-    # get the date range so we have an idea of sampling frequency
-    sql = """SELECT MAX(date) AS max, MIN(date) AS min
-             FROM shipping_clean
-             WHERE id = '%s'
-          """ % id
-    c.execute(sql)
-    (date_max, date_min) = c.fetchone()
-    
     if count is 1:
         # only one sounding, capture as a point
-        sql = """INSERT INTO ship_points VALUES('%s', '%s',
+        (lon, lat, date) = q.pop()
+        sql = """INSERT INTO ship_points
+                 VALUES('%s', '%s',
                    GeometryFromText('POINT(%s)', 4326))
-              """ % (id, date_max, geom.pop())
+              """ % (id, date, "%s %s" % (lon, lat))
+        commit(sql)
+
     else:
-        # multiple soundings, capture linestring
-        sql = """INSERT INTO ship_lines VALUES('%s', %i, '%s', '%s', 
-                   GeometryFromText('MULTILINESTRING((%s))', 4326)) 
-              """ % (id, count, date_min, date_max, ','.join(geom))
-    try:
-        c.execute(sql)
-        print "inserted %s" % id
-    except:
-        conn.rollback()
-        print "borked, trying %s" % sql
-    conn.commit()
+        # multiple soundings, capture linestrings
+        (prev_lon, prev_lat, prev_date) = q[0]
+        for point_row in q:
+            # check for dateline 'wrapping'
+            (cur_lon, cur_lat, cur_date) = point_row
+            if abs(cur_lon - prev_lon) > 180 and \
+               str(cur_lon * prev_lon)[0] is '-':
+                # interpolate two points at the anti-meridian boundry
+
+                cur_s, prev_s = 180, 180 # signs for longitude at boundry
+                if prev_lon < 0:
+                    p_lon = prev_lon + 360
+                    prev_s *= -1
+                else:
+                    p_lon = prev_lon
+                if cur_lon < 0:
+                    c_lon = cur_lon + 360
+                    cur_s *= -1
+                else:
+                    c_lon = cur_lon
+                slope = (prev_lat - cur_lat)/(p_lon - c_lon)
+                s_lat = slope*(180 - p_lon) + prev_lat
+
+                # generate new points based on bountry split
+                p = [ ("%s %s"        % (prev_lon, prev_lat), prev_date), \
+                      ("%3.9f %2.10f" % (prev_s, s_lat),      prev_date), \
+                      ("%3.9f %2.10f" % (cur_s, s_lat),       cur_date),  \
+                      ("%s %s"        % (cur_lon, cur_lat),   cur_date) ]
+                geom += p # add these points to the queue
+
+            else:
+                # push lon lat pair onto geom
+                geom.append(('%s %s' % (cur_lon, cur_lat), cur_date))
+
+            # set lat / lon for next run
+            (prev_lon, prev_lat, prev_date) = point_row
+
+        (p_lonlat, p_date) = geom[0]
+        for g in geom:
+            (c_lonlat, c_date) = g
+            # skip the anti-meridian line
+            cs = c_lonlat.strip('-')
+            ps = p_lonlat.strip('-')
+            
+            if cs != ps:
+                sql = """INSERT INTO ship_lines2 VALUES('%s', %i, '%s', '%s',
+                           GeometryFromText('LINESTRING(%s, %s)', 4326))
+                      """ % (id, count, p_date, c_date,  p_lonlat, c_lonlat)
+                commit(sql)
+            else:
+                if cs[:3] == '180':
+                    print "Skipping anti-meridian case: %s to %s" % (p_lonlat, c_lonlat)
+                else:
+                    sql = """INSERT INTO ship_points
+                             VALUES('%s', '%s',
+                               GeometryFromText('POINT(%s)', 4326))
+                          """ % (id, c_date, c_lonlat)
+                    commit(sql)        
+            (p_lonlat, p_date) = g
 
 print "Done."    
