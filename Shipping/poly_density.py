1a01abdb325674743d590538b2223fad2fd9d48d
Shaun Walbridge <shaun.walbridge@gmail.com>
2007-05-29 18:35:54 +0000

    added all bbox data, poly_density.py for shipping data

--- Shipping/poly_density.py
+++ Shipping/poly_density.py
@@ -6,69 +6,67 @@
  
  To get decent performance, it is absolutely essential that the input vector dataset have a gdal-recognized spatial index (ie a .qix file for shapefiles as created by shtree)
 
- Author: Matthew T. Perry
+ Author: Matthew T. Perry, Shaun C. Walbridge
 
  License: You are free to use or modify this code for any purpose. Any derivative copy must clearly give credit to the original author. This license grants no warranty of any kind, express or implied. 
 """
 import ogr
 import sys
 import os
-import Numeric
-import gdal
+import array
 
 def getOpts():
     poly_ds = sys.argv[1] 
     filename = os.path.basename(os.path.splitext(poly_ds)[0])
-    #poly_ds = "output/extract_clipped/bbox_n178_45.shp"
     poly_lyr = 0
-    #extent = [-180., 40., -175., 50.]
-    cellsize = 0.00833333333333
-    outfile = "shipping_density_%s.tif" % filename
-    format = "GTiff"
-    return (poly_ds,poly_lyr,cellsize,outfile,format)    
+    cellsize = 0.008333333333333333
+    outfile = "shipping_density_%s.asc" % filename
+    return (poly_ds,poly_lyr,cellsize,outfile)    
    
 if __name__ == "__main__":
     # Get the inputs
-    (poly_ds,poly_lyr,cellsize,outfile,format) = getOpts()    
+    (poly_ds,poly_lyr,cellsize,outfile) = getOpts()    
 
     # Get the input layer
     ds = ogr.Open(poly_ds)
     lyr = ds.GetLayer(poly_lyr)
-    
+   
+    # GetExtent returns [minx, miny, maxx, maxy]
     extent = lyr.GetExtent()
+
+    # ASCII file for writing data to
+    asc = open(outfile, 'w')
+
     # Confirm dataset is polygon and extents overlap 
     ydist = extent[3] - extent[2]
     xdist = extent[1] - extent[0]
     
-    xcount = int((xdist/cellsize)+1)
-    ycount = int((ydist/cellsize)+1)
+    xcount = int((xdist/cellsize)) # was +1
+    ycount = int((ydist/cellsize)) # was +1
 
     print xcount, ycount 
+    # write the ASCII Grid header
+    asc.write("ncols %s\n" % xcount)
+    asc.write("nrows %s\n" % ycount)
+    asc.write("xllcorner %s\n" % extent[0])
+    asc.write("yllcorner %s\n" % extent[1])
+    asc.write("cellsize %s\n" % '0.008333333333333')
+    asc.write("nodata_value %s\n" % -9999)
 
-    # Create output raster  
-    driver = gdal.GetDriverByName( format )
-    dst_ds = driver.Create( outfile, xcount, ycount, 1, gdal.GDT_UInt16 )
-
-    # the GT(2) and GT(4) coefficients are zero,     
-    # and the GT(1) is pixel width, and GT(5) is pixel height.     
-    # The (GT(0),GT(3)) position is the top left corner of the top left pixel
-    gt = (extent[0],cellsize,0,extent[3],0,(cellsize*-1.))
-    dst_ds.SetGeoTransform(gt)
+    # map integers to their string versions
+    textlookup = dict([(i, ("%d" %i)) for i in range(-9999, 2**15)])
     
-    dst_band = dst_ds.GetRasterBand(1)
-    dst_band.SetNoDataValue( -9999 )
-
     pixelnum = 0
     
     for ypos in range(ycount):
 	    # Create output line array
-        outArray = Numeric.zeros( (1, xcount) )
-        maxy = extent[3] - ypos * cellsize
-        miny = maxy - cellsize
+        outArray = array.array("h")
         for xpos in range(xcount):
             # create a 4-item list of extents 
             minx = xpos * cellsize + extent[0] 
             maxx = minx + cellsize
+            maxy = extent[3] - ypos * cellsize
+            miny = maxy - cellsize
 
             # Create Polygon geometry from BBOX
             wkt = 'POLYGON ((%f %f, %f %f, %f %f, %f %f, %f %f))' \
@@ -77,7 +75,7 @@ if __name__ == "__main__":
             
             # Set spatial filter
             lyr.SetSpatialFilter(g)
-
+            
             # Loop through all features/geometries w/in filter
             feat = lyr.GetNextFeature()
             feat_count = 0
@@ -92,15 +90,14 @@ if __name__ == "__main__":
             lyr.ResetReading()
 
             #Assign total overlapping polys as value in line array
-            Numeric.put( outArray, xpos, feat_count )
-            
+            outArray.append(feat_count) 
             pixelnum += 1
  
-        dst_band.WriteArray(outArray,0,ypos)
+        textvalues = map(textlookup.__getitem__, outArray.tolist())
+        asc.write(" ".join(textvalues))
+        asc.write("\n")
         pct_complete = (float(pixelnum)/(xcount*ycount) * 100.)
         #if pct_complete >= 10:
         #    sys.exit()
 
         print '%.2f pct complete' % pct_complete
-
-    dst_ds = None
