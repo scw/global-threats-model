7d0e2f373fe8eb4ec02c3d91e1b670adb192e830
Shaun Walbridge <shaun.walbridge@gmail.com>
2007-11-06 15:57:07 +0000

    Added histogram.py, which quickly calculates a histogram for a GRASS raster

---
+++ Threats/histogram.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+"""
+histogram.py
+
+Extracted from monte_carlo_threats.py, this just runs the histogram component within
+a current mapset against the specified raster. The current extent is expected to match
+the region of interest.
+
+Usage: histogram.py grass_raster_name
+
+"""
+
+import os
+import re
+import sys
+import shutil
+
+from os.path import *
+from grass_settings import settings
+
+csv_name = sys.argv[1]
+
+# Calculate the statistics for the resulting threat model
+cmd = 'r.info -r %s' % csv_name
+maxmin = os.popen(cmd).read().split()
+for v in maxmin:
+    (var, val) = v.split("=")
+    exec "%s = %s" % (var, val)
+
+range = max - min
+# bins are of size .01
+bins = int(range * 100)
+
+cmd = "r.stats -c -C %s nsteps=%i output=%s.txt" % (csv_name, bins, csv_name)
+os.popen(cmd)
+
+model_csv = '%s.csv' % csv_name
+
+fin = open('%s.txt' % csv_name, 'r')
+fout = open(model_csv, 'w')
+lines = fin.readlines()
+
+pattern = '^[0-9\.]+-([0-9]+\.[0-9]{2})[0-9]+ ([0-9]+)'
+for line in lines:
+    m = re.search(pattern, line)
+    if m:
+        fout.write("%s,%s\n" % m.groups())
+
+fin.close()
+fout.close()
+
+# copy the CSV to the user's home directory
+shutil.copyfile('./%s' % model_csv, '%s/%s' % (os.environ['HOME'], model_csv))
