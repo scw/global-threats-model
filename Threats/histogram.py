77c85918faad3a7ccad651d51faef30ecbe6213a
Shaun Walbridge <shaun.walbridge@gmail.com>
2007-11-06 22:11:45 +0000

    histogram.py now generates a .log file which contains the pixel count and the cummulative sum, excluding the 0.01 bin which contains the model 0 values.

--- Threats/histogram.py
+++ Threats/histogram.py
@@ -19,7 +19,7 @@ from os.path import *
 from grass_settings import settings
 
 csv_name = sys.argv[1]
-
+"""
 # Calculate the statistics for the resulting threat model
 cmd = 'r.info -r %s' % csv_name
 maxmin = os.popen(cmd).read().split()
@@ -33,21 +33,35 @@ bins = int(range * 100)
 
 cmd = "r.stats -c -C %s nsteps=%i output=%s.txt" % (csv_name, bins, csv_name)
 os.popen(cmd)
-
+"""
 model_csv = '%s.csv' % csv_name
 
 fin = open('%s.txt' % csv_name, 'r')
 fout = open(model_csv, 'w')
+flog = open('%s.log' % csv_name, 'w')
 lines = fin.readlines()
 
 pattern = '^[0-9\.]+-([0-9]+\.[0-9]{2})[0-9]+ ([0-9]+)'
+# store the running totals of pixels and cummulative value
+pixel_count = 0
+sum_count = 0
+
 for line in lines:
     m = re.search(pattern, line)
     if m:
-        fout.write("%s,%s\n" % m.groups())
+        bin_in, count_in = m.groups()
+        bin = float(bin_in)
+        count = int(count_in)
+        # ignore the first bin, it contains our zero values
+        if bin_in != '0.01':
+            pixel_count += count
+            sum_count += bin * count
+        fout.write("%s,%s\n" % (bin, count))
 
+flog.write("pixel count: %s\ncummulative sum: %s\n" % (pixel_count, sum_count))
 fin.close()
 fout.close()
+flog.close()
 
 # copy the CSV to the user's home directory
 shutil.copyfile('./%s' % model_csv, '%s/%s' % (os.environ['HOME'], model_csv))
