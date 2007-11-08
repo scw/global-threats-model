50b08172218f4286458fa83c4bb76e1a11d9d404
Shaun Walbridge <shaun.walbridge@gmail.com>
2007-11-08 01:51:45 +0000

    Histogram.py now generates two output csv files: one with 0.1 bins, the other with 0.01 bins.

--- Threats/histogram.py
+++ Threats/histogram.py
@@ -13,55 +13,58 @@ Usage: histogram.py grass_raster_name
 import os
 import re
 import sys
-import shutil
 
-from os.path import *
+def calculate_histogram(bins, raster, basename, stats = False):
+    cmd = "r.stats -c -C %s nsteps=%i output=%s.txt" % (raster, bins, basename)
+    os.popen(cmd)
 
-csv_name = sys.argv[1]
+    fin = open('%s.txt' % basename, 'r')
+    fout = open('%s.csv' % basename, 'w')
+    if stats:
+        flog = open('%s.log' % basename, 'w')
+        # store the running totals of pixels and cummulative value
+        pixel_count = 0
+        sum_count = 0
 
-# Calculate the statistics for the resulting threat model
-cmd = 'r.info -r %s' % csv_name
-maxmin = os.popen(cmd).read().split()
-for v in maxmin:
-    (var, val) = v.split("=")
-    exec "%s = %s" % (var, val)
+    pattern = '^[0-9\.]+-([0-9]+\.[0-9]{2})[0-9]+ ([0-9]+)'
+    lines = fin.readlines()
 
-range = max - min
-# bins are of size .01
-bins = int(range * 100)
-
-cmd = "r.stats -c -C %s nsteps=%i output=%s.txt" % (csv_name, bins, csv_name)
-os.popen(cmd)
-
-model_csv = '%s.csv' % csv_name
-
-fin = open('%s.txt' % csv_name, 'r')
-fout = open(model_csv, 'w')
-flog = open('%s.log' % csv_name, 'w')
-lines = fin.readlines()
-
-pattern = '^[0-9\.]+-([0-9]+\.[0-9]{2})[0-9]+ ([0-9]+)'
-# store the running totals of pixels and cummulative value
-pixel_count = 0
-sum_count = 0
-
-for line in lines:
-    m = re.search(pattern, line)
-    if m:
-        bin_in, count_in = m.groups()
-        bin = float(bin_in)
-        count = int(count_in)
-        # ignore the first bin, it contains our zero values
-        if bin_in != '0.01':
-            pixel_count += count
-            sum_count += bin * count
+    for line in lines:
+        m = re.search(pattern, line)
+        if m:
+            bin_in, count_in = m.groups()
+            bin = float(bin_in)
+            count = int(count_in)
+            if stats:
+                pixel_count += count
+                sum_count += bin * count
         fout.write("%s,%s\n" % (bin, count))
 
-flog.write("pixel count: %s\ncummulative sum: %s\n" % (pixel_count, sum_count))
-fin.close()
-fout.close()
-flog.close()
+    if stats:
+        flog.write("pixel count: %s\ncummulative sum: %s\n" % (pixel_count, sum_count))
+        flog.close()
 
-# copy the CSV to the user's home directory
-shutil.copyfile('./%s' % model_csv, '%s/%s' % (os.environ['HOME'], model_csv))
+    fin.close()
+    fout.close()
 
+def calculate_range(raster):
+    cmd = 'r.info -r %s' % raster 
+    maxmin = os.popen(cmd).read().split()
+    for v in maxmin:
+        (var, val) = v.split("=")
+        exec "%s = %s" % (var, val)
+
+    range = max - min
+    return range
+
+raster = sys.argv[1]
+
+# Calculate the statistics for the resulting threat model
+range = calculate_range(raster)
+
+# bins are of size .01, .1
+b = {'fine' : int(range * 100),
+     'coarse' : int(range * 10)}
+
+calculate_histogram(b['fine'], raster, "%s_%s" % (raster, 'fine'), True)
+calculate_histogram(b['coarse'], raster, "%s_%s" % (raster, 'coarse'), False)
